3f26d030de9136041991dd49a77ba82ffc85d497
Hà Công Bằng <hacongbang174@gmail.com>
2023-03-08 14:05:01 +0700

    area

--- 1. Introduction to Java/operator-expression/src/main/java/com/codegym/Main.java
+++ 1. Introduction to Java/operator-expression/src/main/java/com/codegym/Main.java
@@ -6,11 +6,15 @@ public class Main {
     public static void main(String[] args) {
         float width;
         float length;
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Nhập chiều rộng (width): ");
         width = scanner.nextFloat();
+
         System.out.println("Nhập chiều dài (length): ");
         length = scanner.nextFloat();
+
         float area = width * length;
         System.out.println("Diện tích là:" + area);
     }
