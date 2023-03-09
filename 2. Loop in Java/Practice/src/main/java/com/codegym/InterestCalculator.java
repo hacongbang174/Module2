6da09aaae9ff4629ab1d124832a4eda0ba3b6467
Hà Công Bằng <hacongbang174@gmail.com>
2023-03-09 11:25:57 +0700

    prtLoop2

--- 2. Loop in Java/Practice/src/main/java/com/codegym/InterestCalculator.java
+++ 2. Loop in Java/Practice/src/main/java/com/codegym/InterestCalculator.java
@@ -4,16 +4,13 @@ import java.util.Scanner;
 
 public class InterestCalculator {
     public static void main(String[] args) {
-        double money = 1.0;
-        int month = 1;
-        double interestRate = 1.0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter investment amount: ");
-        money = scanner.nextDouble();
+        double money = scanner.nextDouble();
         System.out.println("Enter number of months: ");
-        month = scanner.nextInt();
+        int month = scanner.nextInt();
         System.out.println("Enter annual interest rate in percentage: ");
-        interestRate = scanner.nextDouble();
+        double interestRate = scanner.nextDouble();
         double totalInterest = 0;
         for (int i = 0; i < month; i++) {
             totalInterest += money * (interestRate / 100) / 12 * month;
