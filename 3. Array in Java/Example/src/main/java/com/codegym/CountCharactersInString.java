5a51da112d3368aecaae2f597fba3e9ee93331c0
Hà Công Bằng <hacongbang174@gmail.com>
2023-03-10 15:23:20 +0700

    expArray8

---
+++ 3. Array in Java/Example/src/main/java/com/codegym/CountCharactersInString.java
@@ -0,0 +1,34 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class CountCharactersInString {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String myString = input.nextLine();
+
+        int count = 0;
+        String myChar;
+        do {
+            System.out.print("Enter a character you want to find: ");
+            myChar = input.next();
+            if (myChar.length() > 1) {
+                System.out.println("You can only enter one character like this: 'a', '$', '1', ...");
+                continue;
+            }
+            for (int i = 0; i < myString.length(); i++) {
+                if (myChar.charAt(0) == myString.charAt(i)) {
+                    count++;
+                }
+            }
+            break;
+        } while (true);
+
+        if (count == 0) {
+            System.out.printf("Can't find '%s' in your string!", myChar);
+            return;
+        }
+        System.out.printf("There %s %d character '%s' in your string.", count == 1 ? "is" : "are", count, myChar);
+    }
+}
