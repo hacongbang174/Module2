0b840ed6d73cf3c1c92528761b052a71a7f33257
Hà Công Bằng <hacongbang174@gmail.com>
2023-03-12 11:52:33 +0700

    Access modifier 2

---
+++ 5. Access modifier, static method, static property/Example/Student/src/main/java/org/example/TestStudent.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        Student stu1 = new Student();
+        stu1.setName("Gorge");
+        stu1.setClasses("G06");
+    }
+}
