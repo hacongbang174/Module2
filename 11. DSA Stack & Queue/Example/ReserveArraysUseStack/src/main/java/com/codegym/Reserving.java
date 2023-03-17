56bb3e1ca1340039a1155482084ee8321de23fd3
Hà Công Bằng <hacongbang174@gmail.com>
2023-03-17 15:33:33 +0700

    reserve

--- 11. DSA Stack & Queue/Example/ReserveArraysUseStack/src/main/java/com/codegym/Reserving.java
+++ 11. DSA Stack & Queue/Example/ReserveArraysUseStack/src/main/java/com/codegym/Reserving.java
@@ -3,32 +3,32 @@ package com.codegym;
 import java.util.Arrays;
 import java.util.Stack;
 public class Reserving {
-    private static int[] reserveIntArray(int[] array) {
-        Stack<Integer> myStack = new Stack<>();
-        for (int element : array) {
-            myStack.push(element);
+    public static int[] reserveArray (int[] array) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < array.length; i++) {
+            stack.push(array[i]);
         }
         for (int i = 0; i < array.length; i++) {
-            array[i] = myStack.pop();
+            array[i] = stack.pop();
         }
         return array;
     }
-
-    private static String reserveString(String string) {
-        Stack<Character> myStack = new Stack<>();
+    public static String reserveString (String string) {
+        Stack<Character> wStack = new Stack<>();
         for (int i = 0; i < string.length(); i++) {
-            myStack.push(string.charAt(i));
+            wStack.push(string.charAt(i));
         }
-        String output = "";
+        String display = "";
         for (int i = 0; i < string.length(); i++) {
-            output += myStack.pop();
+            display += wStack.pop();
         }
-        return output;
+        return display;
     }
 
     public static void main(String[] args) {
-        int[] myArray = reserveIntArray(new int[] {1,2,3,4,5,6,7,8});
-        System.out.println(Arrays.toString(myArray));
-        System.out.println(reserveString("BornToShine"));
+        int[] arr = {1,2,3,4,7,8,9};
+        int [] array = reserveArray(arr);
+        System.out.println(Arrays.toString(array));
+        System.out.println(reserveString("Codegym"));
     }
 }
