6bc050f9903f1bce9d8d50a30425f4ab9e2b6c63
Hà Công Bằng <hacongbang174@gmail.com>
2023-03-20 15:21:13 +0700

    InsertionSort

---
+++ 14. Sort Algorithm/Example/Sort/src/main/java/com/codegym/InsertionSort.java
@@ -0,0 +1,38 @@
+package com.codegym;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập size: ");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+
+        insertionSort(arr);;
+    }
+    public static void insertionSort(int[] array){
+        int pos, x;
+        for(int i = 1; i < array.length; i++){ //đoạn array[0] đã sắp xếp
+            x = array[i];
+            pos = i;
+            while(pos > 0 && x < array[pos-1]){
+                array[pos] = array[pos-1]; // đổi chỗ
+                System.out.println("Swap " + x + " with " + array[pos]);
+                pos--;
+            }
+            array[pos] = x;
+
+            System.out.print("List after the  " + i + "' sort: ");
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
