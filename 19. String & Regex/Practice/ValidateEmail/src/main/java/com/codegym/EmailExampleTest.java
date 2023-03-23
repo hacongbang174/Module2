692841ea77c1770b748da26cd116b73c9d8eaa0d
unknown <hacongbang174@gmail.com>
2023-03-23 20:42:08 +0700

    validEmail

--- 19. String & Regex/Practice/ValidateEmail/src/main/java/com/codegym/EmailExampleTest.java
+++ 19. String & Regex/Practice/ValidateEmail/src/main/java/com/codegym/EmailExampleTest.java
@@ -2,7 +2,7 @@ package com.codegym;
 
 public class EmailExampleTest {
     private static EmailExample emailExample;
-    public static final String[] validEmail = new String[]{"a@gmail.com", "ab@yahoo.com", "abc@hotmail.com"};
+    public static final String[] validEmail = new String[]{"a3@gmail.com", "ab@yahoo.com", "ab11c@hotmail.com"};
     public static final String[] invalidEmail = new String[]{"@gmail.com", "ab@gmail.", "@#abc@gmail.com"};
 
     public static void main(String[] args) {
