6e8b77de3dbf9b6453df407c3dbdf2ba2f1790ea
unknown <hacongbang174@gmail.com>
2023-03-23 18:31:21 +0700

    oddeven

---
+++ 18. Thread/Example/OldEvent/src/main/java/com/codegym/EvenThread.java
@@ -0,0 +1,8 @@
+package com.codegym;
+
+public class EvenThread implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
---
+++ 18. Thread/Example/OldEvent/src/main/java/com/codegym/OddThread.java
@@ -0,0 +1,8 @@
+package com.codegym;
+
+public class OddThread implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
