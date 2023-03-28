b6bd91766099b57a27b118b236f03d71379c42ad
hacongbang174 <hacongbang174@gmail.com>
2023-03-28 13:54:09 +0700

    case

--- CaseStudy/src/main/java/model/User.java
+++ CaseStudy/src/main/java/model/User.java
@@ -1,7 +1,5 @@
 package model;
 
-import javax.management.relation.Role;
-
 public class User {
     private int id;
     private String username;
@@ -26,4 +24,93 @@ public class User {
         this.role = role;
     }
 
+    public User(String raw) {
+        String [] strings = raw.split(",");
+        this.id = Integer.parseInt(strings[0]);
+        this.username = strings[1];
+        this.password = strings[2];
+        this.name = strings[3];
+        this.phoneNumber = strings[4];
+        this.email = strings[5];
+        this.address = strings[6];
+        this.role = Role.valueOf(strings[7]);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", role=" + role +
+                '}';
+    }
 }
